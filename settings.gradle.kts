117705b5a298f2b995951517a20abf529eeadc80
bilaapd <114081903+bilaapd@users.noreply.github.com>
2024-02-05 21:02:26 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Settingpage_TETI"
+include(":app")
+ 
